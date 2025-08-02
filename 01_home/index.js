4ed6b918cc52793c7b6befe22898375f6e3c24af
Priyanshu <04priyanshuuuu@gmail.com>
2025-08-02 14:03:24 +0530

    updated again

--- 01_home/index.js
+++
@@ -1,9 +0,0 @@
-import { auth } from "/04_Backend/backend.js";
-import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-auth.js";
-
-onAuthStateChanged(auth, (user) => {
-    if (!user) {
-        // ❌ Not logged in → send back to login
-        window.location.href = "../03_login/login.html";
-    }
-});
