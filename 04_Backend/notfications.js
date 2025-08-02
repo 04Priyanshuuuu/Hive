b045ea124ee998a9c14cc92fd3fb664535870e55
Priyanshu <04priyanshuuuu@gmail.com>
2025-08-02 13:42:25 +0530

    js update

--- 04_Backend/notfications.js
+++
@@ -1,8 +0,0 @@
-onSnapshot(collection(db, "events"), (snapshot) => {
-    const notifSection = document.getElementById("notifications"); // id="notifications"
-    notifSection.innerHTML = "";
-    snapshot.forEach((docu) => {
-        const data = docu.data();
-        notifSection.innerHTML += `<p><strong>${data.title}</strong> on ${data.date}</p>`;
-    });
-});
